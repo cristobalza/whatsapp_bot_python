70e0ca08543d9d3a969573e63588f62ba5be535d
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-08-20 21:08:13 -0700

    first edits after heroku depluy

--- main.py
+++ main.py
@@ -8,5 +8,19 @@ app = Flask(__name__)
 def hola():
     return "Hola, Mundo!"
 
+@app.route("/whatsapp", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+
+    msg = request.form.get('Body')
+
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
