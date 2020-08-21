39e1afd00f124910d9c5b2ed7e513308b0518fc2
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-08-20 18:33:55 -0700

    really hot day

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Whatsapp Bot
 
 ![api_bot](imgs/regular_api.png)
+
+![api_bot_2](imgs/with_twilio.png)
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import os
-from twilio.rest import Client
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/")
+def hola():
+    return "Hola, Mundo!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
