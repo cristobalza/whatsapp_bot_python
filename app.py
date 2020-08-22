9a65632b3d684d1f2a2bb360b401a0ade0646853
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-08-22 15:14:20 -0700

    go back

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
-from tools import fetch_reply
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def hola():
+    return "Hola, Mundo!"
 
 @app.route("/whatsapp", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
-    phone_no = request.form.get('From')
-    
-    reply = fetch_reply(msg, phone_no)
 
-    # Create reply
+    msg = request.form.get('Body')
+
     resp = MessagingResponse()
-    resp.message(reply)
+    resp.message("You said: {}".format(msg))
 
     return str(resp)
 
