92fadbfe540ac7422dfa3fdde31bbed7a53a79e5
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-08-23 14:31:26 -0700

    added response

--- app.py
+++ app.py
@@ -37,25 +37,30 @@ def create():
     except InvalidArgument:
         raise
 
-    print('Query Text:', response.query_result.query_text)
-    print('Detected intent', response.query_result.intent.display_name)
-    print('Detected intent confidence:', response.query_result.intent_detection_confidence)
-    print('Fulfillmnet text:', response.query_result.fulfillment_text)
-    # sendMessage()
-    return response.query_result.fulfillment_text
-    
-def whatsapp_reply(phone_num, message):
-    
-    url = "https://api.twilio.com/2010-04-01/Accounts/ACb4f3673da88894b59e7943e7051cfe39/Messages.json"
+    # print('Query Text:', response.query_result.query_text)
+    # print('Detected intent', response.query_result.intent.display_name)
+    # print('Detected intent confidence:', response.query_result.intent_detection_confidence)
+    # print('Fulfillmnet text:', response.query_result.fulfillment_text)
 
-    payload = {'From': 'whatsapp:'+ my_phone_number,
-    'Body':message,
-    'To':phone_num
-    }
+    reply_diagfl = response.query_result.fulfillment_text
+    resp_twi = MessagingResponse()
+    resp_twi.message(reply_diagfl)
+
+    return str(resp_twi)
+
     
-    headers = {'Authorization': 'Basic QUNiNGYzNjczZGE4ODg5NGI1OWU3OTQzZTcwNTFjZmUzOTpkOWI4N2VjOTY5OTk1ZDc3MDliNDg3OWEzY2U5ZWNkNQ=='}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    print(response.text.encode('utf8'))
-    return ""
+# def whatsapp_reply(phone_num, message):
+    
+#     url = "https://api.twilio.com/2010-04-01/Accounts/ACb4f3673da88894b59e7943e7051cfe39/Messages.json"
+
+#     payload = {'From': 'whatsapp:'+ my_phone_number,
+#     'Body':message,
+#     'To':phone_num
+#     }
+    
+#     headers = {'Authorization': 'Basic QUNiNGYzNjczZGE4ODg5NGI1OWU3OTQzZTcwNTFjZmUzOTpkOWI4N2VjOTY5OTk1ZDc3MDliNDg3OWEzY2U5ZWNkNQ=='}
+#     response = requests.request("GET", url, headers=headers, data = payload)
+#     print(response.text.encode('utf8'))
+#     return ""
 if __name__ == "__main__":
     app.run()
