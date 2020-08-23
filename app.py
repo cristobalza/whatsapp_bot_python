934e0cb94b014ccd4a8ba9460eb9f773df7cb468
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-08-23 13:36:27 -0700

    finally dialogflow integration is working fine

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
 import os
+import dialogflow_v2 as dialogflow 
+# from dialogflow_v2.types import TextInput, QueryInput
+
+
+from google.api_core.exceptions import InvalidArgument
+import requests
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'private-key.json'
+
+DIALOGFLOW_PROJECT_ID = 'whatsapp-project-1'
+DIALOGFLOW_LANGUAGE_CODE = 'en'
+SESSION_ID = 'me'
+
+# from utils import fetch_reply
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route("/")
-def hola():
+def root():
     return "Hola, Mundo!"
 
 @app.route("/whatsapp", methods=['POST'])
-def sms_reply():
+def whatsapp_reply():
     """Respond to incoming calls with a simple text message."""
-
     msg = request.form.get('Body')
+    phone_num = request.form.get('From')
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.types.TextInput(text=msg, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
 
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
-
-    return str(resp)
+    print('Query Text:', response.query_result.query_text)
+    print('Detected intent', response.query_result.intent.display_name)
+    print('Detected intent confidence:', response.query_result.intent_detection_confidence)
+    print('Fulfillmnet text:', response.query_result.fulfillment_text)
+    # sendMessage()
+    return response.query_result.fulfillment_text
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- utils.py
+++ utils.py
@@ -2,9 +2,8 @@ import os
 import dialogflow_v2 as dialogflow
 from dialogflow_v2.types import TextInput, QueryInput
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "secure-site-280108-a72354178baf.json"
 dialogflow_session_client = dialogflow.SessionsClient()
-PROJECT_ID="secure-site-280108"
+
 
 
 def detect_intent_from_text(text, session_id, language_code='en'):
@@ -15,7 +14,7 @@ def detect_intent_from_text(text, session_id, language_code='en'):
     return response.query_result
 
 
-def fetch_reply(query, session_id):
+def construct_reply(query, session_id):
     """
     Takes a query and returns in a text form
     """
